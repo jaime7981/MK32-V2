eedee4d3bbabc1892a447dac74fce0b1d19bea70
jaime7981 <jaimemanuelf99@gmail.com>
2024-12-22 17:40:58 -0300

    refactor: replace deprecated esp_adc_cal reference for esp_adc

--- components/battery/battery_monitor.c
+++ components/battery/battery_monitor.c
@@ -29,7 +29,7 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_adc.h"
 #include "keyboard_config.h"
 
 #include "battery_monitor.h"
